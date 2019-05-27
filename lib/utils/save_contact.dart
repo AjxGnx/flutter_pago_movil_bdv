d3e876914ecab14515aae73571fd75fa439e40c3
alirio gutierrez <alirio1925@gmail.com>
2019-05-27 18:01:11 -0400

    [ADD] se agrego funcion para guardar contactos

---
+++ lib/utils/save_contact.dart
@@ -0,0 +1,24 @@
+import 'package:pago_movil_bdv/setting/database_client.dart';
+import 'package:pago_movil_bdv/models/Contact.dart';
+
+void save_contact({String codeBank, String codeMobile, String mobile, String dni, String name}){
+
+  final DatabaseClient db = DatabaseClient();
+  db.create().then((dd){
+    ContactModel contact = new ContactModel();
+    contact.name = name;
+    contact.phone = '$codeMobile$mobile';
+    contact.codeBank = codeBank;
+    contact.dni= dni;
+
+    db.upsertContact(contact).then((ContactModel contact){
+    });
+
+    Future<List<ContactModel>> latestContacts =  db.fetchLatestContact();
+
+    latestContacts.then((dd){
+      print(dd.first.toMap());
+    });
+  });
+
+}
