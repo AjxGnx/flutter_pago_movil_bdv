aabd4007b2b9d08e8c466b6b6c6717a7b5695fe9
alirio gutierrez <alirio1925@gmail.com>
2019-05-28 01:35:23 -0400

    [MOD] se modifico el widget

---
+++ lib/pages/contact_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pago_movil_bdv/models/Contact.dart';
+
+class ContactItem extends StatelessWidget {
+  final ContactModel _contact;
+  ContactItem(this._contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: (){
+        print(_contact.name);
+      },
+      leading: CircleAvatar(
+        backgroundColor: Colors.redAccent,
+        foregroundColor: Colors.white,
+        child: Text(_contact.name[0]),
+      ),
+      title: Text(_contact.name),
+      subtitle: Text(_contact.codeBank+' '+ _contact.phone+' '+ _contact.dni),
+    );
+  }
+}
