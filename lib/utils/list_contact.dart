219d066c13bb0c9727547b4551a64074e22affbb
alirio gutierrez <alirio1925@gmail.com>
2019-05-28 01:37:04 -0400

    [ADD] se agrego funcion para consultar database

---
+++ lib/utils/list_contact.dart
@@ -0,0 +1,13 @@
+import 'package:pago_movil_bdv/setting/database_client.dart';
+import 'package:pago_movil_bdv/models/Contact.dart';
+
+list_contact(){
+
+  final DatabaseClient db = DatabaseClient();
+  return db.create().then((data) async{
+
+    List<ContactModel> latestContacts =  await db.fetchLatestContact();
+    return (latestContacts);
+    });
+  }
+
