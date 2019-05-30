e2172f83e90ee527d1eaa6ee3032d7fc44b95848
alirio gutierrez <alirio1925@gmail.com>
2019-05-30 14:02:40 -0400

    [ADD] se anadio form para validar campos

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:pago_movil_bdv/data/bank_code.dart';
 import 'package:pago_movil_bdv/data/mobile_code.dart';
 import 'package:pago_movil_bdv/pages/add_name_contact.dart';
 import 'package:pago_movil_bdv/pages/contact_list.dart';
+import 'package:pago_movil_bdv/utils/delete_contact.dart';
 import 'package:pago_movil_bdv/utils/list_contact.dart';
 
 void main() {
@@ -30,9 +31,8 @@ class _HomeState extends State<Home> {
   );
 
   void _valueChanged(bool value) => setState(() => _chexbox = value);
-
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
+  final _formKey = GlobalKey<FormState>();
 
 
 
@@ -46,170 +46,197 @@ class _HomeState extends State<Home> {
         .of(context)
         .size
         .height;
-
-
-
     return Scaffold(
       key: _scaffoldKey,
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: Text('Pago Clave BDV'),
         ),
         body: ListView(
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: _screenAlto / 20),
+            Form(
+              key: _formKey,
               child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.only(bottom: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.only(left: 5),
-                            height: 59,
-                            decoration: BoxDecoration(
-                                border:
-                                Border.all(color: Colors.black45, width: 1),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: DropdownButtonHideUnderline(
-                              child: DropdownButton(
-                                value: _codeBank,
-                                items: listCodeBank,
-                                hint: Text('Banco Receptor'),
-                                onChanged: (value) {
-                                  _codeBank = value;
-                                  setState(() {});
+                padding: EdgeInsets.only(top: _screenAlto / 20),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.only(left: 10),
+                                width: _screenAncho/1.25,
+                                child: TextFormField(
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Completa este campo';
+                                    }
+                                  },
+                                  decoration: InputDecoration(
+                                    labelText: 'Cedula',
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                  ),
+                                  controller: _dniController,
+                                )),
+                            Container(
+                              padding: EdgeInsets.only(right: 10,left: 22),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.contacts,
+                                  color: Colors.redAccent,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ContactList()
+                                          ));
                                 },
                               ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 15),
-                            child: IconButton(
-                              icon: Icon(
-                                Icons.contacts,
-                                color: Colors.redAccent,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.only(left: 5, right: 14),
+                                child: Container(
+                                  width: _screenAncho / 2.1,
+                                  padding: EdgeInsets.only(left: 5),
+                                  child: DropdownButtonHideUnderline(
+                                    child: DropdownButtonFormField(
+                                      decoration: InputDecoration(
+                                          border: OutlineInputBorder(
+                                              borderRadius:
+                                              BorderRadius.all(Radius.circular(10)))
+                                      ),
+                                      value: _codeMobile,
+                                      items: listCodeMobile,
+                                      hint: Text('Prefijo'),
+                                      onChanged: (value) {
+                                        _codeMobile = value;
+                                        setState(() {
+                                        });
+                                      },
+                                      validator: (value){
+                                      if (value == null) {
+                                        return 'Completa este campo';
+                                      }
+                                    } ,
+                                    ),
+                                  ),
+                                )),
+                            Container(
+                              padding: EdgeInsets.only(right: 10),
+                              width: _screenAncho / 2.1,
+                              child: TextFormField(
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Completa este campo';
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  labelText: 'Numero celular',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                                ),
+                                controller: _mobileController,
                               ),
-                              onPressed: () {
+                            )
+                          ],
+                        ),
+                      ),
+                  Container(
+                    padding: EdgeInsets.only(left: 10,top: 20,right: 10),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButtonFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(10)))
+                        ),
+                        value: _codeBank,
+                        items: listCodeBank,
+                        hint: Text('Banco Receptor'),
+                        onChanged: (value) {
+                          _codeBank = value;
+                          setState(() {});
+                        },
+                        validator: (value){
+                          if (value == null) {
+                            return 'Completa este campo';
+                          }
+                        }
+                      ),
+                    ),
+                  ),
+                      Container(
+                          padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Completa este campo';
+                              }
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Monto',
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                            ),
+                          )),
+                      Container(
+                        child: CheckboxListTile(
+                          value: _chexbox,
+                          onChanged: _valueChanged,
+                          title: new Text('Agregar a pagos frecuentes?'),
+                          activeColor: Colors.red,
+                        ),
+                      ),
+                      Container(
+                        width: 120,
+                        height: 60,
+                        padding: EdgeInsets.only(top: 20),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: RaisedButton(
+                            color: Colors.redAccent,
+                            child: Text(
+                              'PAGAR',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              if (_chexbox == true) {
                                 Navigator.push(
                                     context,
                                     MaterialPageRoute(
                                         builder: (context) =>
-                                            ContactList(list_contact())
-                                        ));
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                              padding: EdgeInsets.only(left: 5, right: 5),
-                              child: Container(
-                                width: _screenAncho / 2.1,
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.black45, width: 1),
-                                    borderRadius: BorderRadius.circular(10)),
-                                padding: EdgeInsets.only(left: 5),
-                                height: 61,
-                                child: DropdownButtonHideUnderline(
-                                  child: DropdownButton(
-                                    value: _codeMobile,
-                                    items: listCodeMobile,
-                                    hint: Text('Prefijo'),
-                                    onChanged: (value) {
-                                      _codeMobile = value;
-                                      setState(() {
-                                        print(value);
-                                      });
-                                    },
-                                  ),
-                                ),
-                              )),
-                          Container(
-                            padding: EdgeInsets.only(left: 5, right: 5),
-                            width: _screenAncho / 2.1,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                labelText: 'Numero celular',
-                                border: OutlineInputBorder(
-                                    borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                              ),
-                              controller: _mobileController,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                        padding: EdgeInsets.only(left: 10, right: 9, top: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Cedula',
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
+                                            AddNameContact({
+                                              'codeBank': _codeBank,
+                                              'codeMobile': _codeMobile,
+                                              'mobile': _mobileController.text,
+                                              'dni': _dniController.text,
+                                            })));
+                              }else{
+                                _scaffoldKey.currentState.showSnackBar(snackBar);
+                              }
+                            }},
                           ),
-                          controller: _dniController,
-                        )),
-                    Container(
-                        padding: EdgeInsets.only(left: 10, right: 9, top: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Monto',
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                          ),
-                        )),
-                    Container(
-                      child: CheckboxListTile(
-                        value: _chexbox,
-                        onChanged: _valueChanged,
-                        title: new Text('Agregar a pagos frecuentes?'),
-                        activeColor: Colors.red,
-                      ),
-                    ),
-                    Container(
-                      width: 120,
-                      height: 60,
-                      padding: EdgeInsets.only(top: 20),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: RaisedButton(
-                          color: Colors.redAccent,
-                          child: Text(
-                            'PAGAR',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () {
-                            if (_chexbox == true) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          AddNameContact({
-                                            'codeBank': _codeBank,
-                                            'codeMobile': _codeMobile,
-                                            'mobile': _mobileController.text,
-                                            'dni': _dniController.text,
-                                          })));
-                            }else{
-                              _scaffoldKey.currentState.showSnackBar(snackBar);
-                            }
-                          },
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             )
